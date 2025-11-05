80ede2282052af48611558baaedf3253f77aca14
Wilie12 <albertw536@gmail.com>
2025-11-05 20:50:40 +0100

    Implemented functionality to check grammar of already uploaded file

--- src/main/java/com/nn/spring_note_rest_api/note/controller/NoteController.java
+++ src/main/java/com/nn/spring_note_rest_api/note/controller/NoteController.java
@@ -1,7 +1,10 @@
 package com.nn.spring_note_rest_api.note.controller;
 
+import com.nn.spring_note_rest_api.language.api.response.LanguageResponse;
+import com.nn.spring_note_rest_api.language.service.LanguageService;
 import com.nn.spring_note_rest_api.note.api.response.NoteResponse;
 import com.nn.spring_note_rest_api.note.service.NoteService;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/notes")
 public class NoteController {
     private final NoteService noteService;
+    private final LanguageService languageService;
 
-    public NoteController(NoteService noteService) {
+    public NoteController(NoteService noteService, LanguageService languageService) {
         this.noteService = noteService;
+        this.languageService = languageService;
     }
 
     @PostMapping
@@ -23,5 +28,16 @@ public class NoteController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(noteResponse);
+    }
+
+    @GetMapping("/check/{noteId}")
+    public ResponseEntity<LanguageResponse> checkNoteGrammar(@PathVariable long noteId) {
+        Resource note = noteService.getNoteResource(noteId);
+
+        LanguageResponse languageResponse = languageService.checkGrammar(note);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(languageResponse);
     }
 }
