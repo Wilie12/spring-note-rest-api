a71137cc28695e152cebc3605653dee1e5c4da68
Wilie12 <albertw536@gmail.com>
2025-11-06 20:25:46 +0100

    Implemented functionality to render note into HTML page with HTML template

--- src/main/java/com/nn/spring_note_rest_api/note/controller/NoteController.java
+++ src/main/java/com/nn/spring_note_rest_api/note/controller/NoteController.java
@@ -3,25 +3,36 @@ package com.nn.spring_note_rest_api.note.controller;
 import com.nn.spring_note_rest_api.language.api.response.LanguageResponse;
 import com.nn.spring_note_rest_api.language.service.LanguageService;
 import com.nn.spring_note_rest_api.note.api.response.NoteResponse;
+import com.nn.spring_note_rest_api.note.domain.NoteMetadata;
+import com.nn.spring_note_rest_api.note.service.MarkdownService;
 import com.nn.spring_note_rest_api.note.service.NoteService;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 @RequestMapping("/notes")
 public class NoteController {
     private final NoteService noteService;
     private final LanguageService languageService;
+    private final MarkdownService markdownService;
 
-    public NoteController(NoteService noteService, LanguageService languageService) {
+    public NoteController(
+            NoteService noteService,
+            LanguageService languageService,
+            MarkdownService markdownService
+    ) {
         this.noteService = noteService;
         this.languageService = languageService;
+        this.markdownService = markdownService;
     }
 
     @PostMapping
+    @ResponseBody
     public ResponseEntity<NoteResponse> uploadNote(@RequestParam("file") MultipartFile file) {
         NoteResponse noteResponse = noteService.uploadNote(file);
 
@@ -31,6 +42,7 @@ public class NoteController {
     }
 
     @GetMapping("/check/{noteId}")
+    @ResponseBody
     public ResponseEntity<LanguageResponse> checkNoteGrammar(@PathVariable long noteId) {
         Resource note = noteService.getNoteResource(noteId);
 
@@ -39,5 +51,16 @@ public class NoteController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(languageResponse);
+    }
+
+    @GetMapping("/{noteId}")
+    public String showNote(@PathVariable long noteId, Model model) {
+        Resource note = noteService.getNoteResource(noteId);
+        NoteMetadata noteMetadata = noteService.getNoteMetadata(noteId);
+        String html = markdownService.resourceToHtml(note);
+
+        model.addAttribute("title", noteMetadata.getOriginalName());
+        model.addAttribute("markdown", html);
+        return "note";
     }
 }
