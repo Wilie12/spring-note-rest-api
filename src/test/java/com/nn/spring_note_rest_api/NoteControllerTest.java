2f6f47e86472b5119ca97b048071547e151c76b9
Wilie12 <albertw536@gmail.com>
2025-11-08 00:22:48 +0100

    Implemented test of checkNoteGrammar method in NoteController

--- src/test/java/com/nn/spring_note_rest_api/NoteControllerTest.java
+++ src/test/java/com/nn/spring_note_rest_api/NoteControllerTest.java
@@ -7,16 +7,18 @@ import com.nn.spring_note_rest_api.note.service.NoteService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.multipart.MultipartFile;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(NoteController.class)
@@ -49,5 +51,18 @@ public class NoteControllerTest {
                 .andExpect(status().isOk());
 
         verify(noteService).uploadNote(multipartFile);
+    }
+
+    @Test
+    public void checkNoteGrammarShouldWork() throws Exception {
+        String fileContent = "Test file content.";
+        Resource resource = new ByteArrayResource(fileContent.getBytes());
+        when(noteService.getNoteResource(any())).thenReturn(resource);
+
+        mvc.perform(get("/api/v1/notes/check/1"))
+                        .andExpect(status().isOk());
+
+        verify(noteService).getNoteResource(1L);
+        verify(languageService).checkGrammar(resource);
     }
 }
